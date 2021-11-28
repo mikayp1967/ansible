a005039c3cb28f85369a12e923c5cb22ea909462
MrP <mikayp1967@gmail.com>
2021-11-28 13:38:15 +0000

    Change start-clients.sh to have function, add diff servers and write server list to file

--- testenv/start-clients.sh
+++ testenv/start-clients.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
 # Spin up small test env for trying out some ansible
+
 #
 
-# Eventually should add config like BASE_PORT, number, IMAGE etc from a config file or something
 
 BASE_PORT=2200
+cat /dev/null > server_list
 
-for CLIENT in `seq 1 5`
-do 
-	PORT=$(( ${BASE_PORT} + ${CLIENT} ))
-	docker run -d --rm -p ${PORT}:22 --name server_${CLIENT} --network=test-net my-ansi-client
-done
+function add_containers() {
+# Create number of containers with specified name
+# add_containers <num to add> <name prefix>
+	for CLIENT in `seq 1 $1`
+	do 
+		BASE_PORT=$(( ${BASE_PORT} + 1 ))
+		docker run -d --rm -p ${BASE_PORT}:22 --name $2_${CLIENT} --network=test-net my-ansi-client
+		printf "$2_${CLIENT}\n" >> server_list
+	done
+}
 
-# What the hell, lets do some "appservers"
-BASE_PORT=2220
-for CLIENT in `seq 1 4`
-do 
-	PORT=$(( ${BASE_PORT} + ${CLIENT} ))
-	docker run -d --rm -p ${PORT}:22 --name app_server_${CLIENT} --network=test-net my-ansi-client
-done
-
-
-# Lets go crazy, lets do some "app2servers"
-BASE_PORT=2240
-for CLIENT in `seq 1 3`
-do 
-	PORT=$(( ${BASE_PORT} + ${CLIENT} ))
-	docker run -d --rm -p ${PORT}:22 --name app2_server_${CLIENT} --network=test-net my-ansi-client
-done
+add_containers 6 server_web
+add_containers 2 server_app_auth
+add_containers 3 server_app_order
+add_containers 2 server_app_charge
 
