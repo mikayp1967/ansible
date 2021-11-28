845824ce39a202e3050386bb49c23b9cb2d44c6c
MrP <mikayp1967@gmail.com>
2021-11-28 10:37:59 +0000

    Add python/bash to client image, tweak stopenv.sh script

--- testenv/stopenv.sh
+++ testenv/stopenv.sh
@@ -3,7 +3,7 @@
 
 # Delete all containers started for this project
 
-for CONT in `docker ps|grep my-ansi-client|grep server_|awk '{print $1}'`
+for CONT in `docker ps|grep server_|awk '{print $1}'`
 do
     docker stop ${CONT}
 done
