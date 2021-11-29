cf05ccbb5d2d7d3bc708529b513a726321b74cb9
MrP <mikayp1967@gmail.com>
2021-11-29 17:14:44 +0000

    Share volume with ansible master, create inventory file of servers/groups

--- testenv/start-clients.sh
+++ testenv/start-clients.sh
@@ -11,12 +11,18 @@ cat /dev/null > server_list
 function add_containers() {
 # Create number of containers with specified name
 # add_containers <num to add> <name prefix>
+
+GROUP_NAME=$(echo $2|sed 's/server_//')
+printf "[${GROUP_NAME}]\n" >> server_list
 	for CLIENT in `seq 1 $1`
 	do 
 		BASE_PORT=$(( ${BASE_PORT} + 1 ))
-		docker run -d --rm -p ${BASE_PORT}:22 --name $2_${CLIENT} --network=test-net my-ansi-client
+		docker run -d --rm -p ${BASE_PORT}:22 --name $2_${CLIENT} --network=test-net -e HNAME=$2_${CLIENT} my-ansi-client
 		printf "$2_${CLIENT}\n" >> server_list
 	done
+	printf "\n" >> server_list
+	cp server_list ../mountdir
+
 }
 
 add_containers 6 server_web
--- testenv/startenv.sh
+++ testenv/startenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker network create -d bridge test-net
-docker run -d --network=test-net --rm my-ansible 
+docker run -d --network=test-net --rm -v /home/ec2-user/repos/ansible/mountdir:/home/mgtuser/mountdir:  my-ansible 
